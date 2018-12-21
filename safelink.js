74cd3d53e41b0d1085a7364691a6ba46cd3add61
Blog HF <bloghf14@gmail.com>
2018-12-21 17:25:19 +0700

    Update safelink.js

--- safelink.js
+++ safelink.js
@@ -23,9 +23,8 @@ var x=Math.floor((Math.random()*15)+ 1);
 var xxx=null;
 if(x=="1"){xxx="2018/12/how-to-use-google-drive-for-work.html"}
 if(x=="2"){xxx="2018/12/10-tips-to-build-startup-business.html"}
-if(x=="3"){xxx="2016/09/auto-insurance-quotes-2015.html"}
-if(x=="4"){xxx="2018/12/how-to-start-business-at-home.html"}
-if(x=="5"){xxx="2018/12/what-is-cryptocurrency.html"}
+if(x=="3"){xxx="2018/12/how-to-start-business-at-home.html"}
+if(x=="4"){xxx="2018/12/what-is-cryptocurrency.html"}
 a_to_ve[i].href="https://intolink.blogspot.com/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
