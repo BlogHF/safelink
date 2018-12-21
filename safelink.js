05c769e0f41a12961d12e855ec88b05dbe121a04
Blog HF <bloghf14@gmail.com>
2018-12-21 17:44:29 +0700

    Update safelink.js

--- safelink.js
+++ safelink.js
@@ -19,14 +19,12 @@ var a_to_ve="";var a_to_vf=new Array();var a_to_vg=0;a_to_ve=document.getElement
 j++;}
 if(a_to_vh==false)
 {var encryptedUrl=Base64.encode(a_to_vi);
-var x=Math.floor((Math.random()*15)+ 1);
+var x=Math.floor((Math.random()*4)+ 1);
 var xxx=null;
-if(x=="1"){xxx="2018/12/how-to-use-google-drive-for-work.html"}
-if(x=="2"){xxx="2018/12/10-tips-to-build-startup-business.html"}
-if(x=="3"){xxx="2018/12/how-to-start-business-at-home.html"}
-if(x=="4"){xxx="2018/12/what-is-cryptocurrency.html"}
-if(x=="5"){xxx="2018/12/what-is-remote-desktop-protection.html"}
-a_to_ve[i].href="https://intolink.blogspot.com/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="1"){xxx="the-way-to-start-business-at-home.html"}
+if(x=="2"){xxx="10-tips-for-start-small-business-startup.html"}
+if(x=="3"){xxx="advantages-of-being-home-business-owner.html"}
+if(x=="4"){xxx="practice-environmental-qwareness-to-our.html"}a_to_ve[i].href="https://www.kompisafelinkv2.ga/2016/12/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
